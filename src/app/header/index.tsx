5c8eb2a49cf633586f16897d6d758c7796a75cc5
kathryngraysonnanz <kathrynkaygrayson@gmail.com>
2025-06-18 15:05:02 -0400

    added basic calendar view and header

---
+++ src/app/header/index.tsx
@@ -0,0 +1,24 @@
+"use client"
+import React from 'react'
+import { useRouter } from 'next/navigation';
+import Link from 'next/link';
+
+import { Button } from "@progress/kendo-react-buttons";
+import { 
+  AppBar,
+  AppBarSection,
+  AppBarSpacer
+} from "@progress/kendo-react-layout";
+import { userIcon } from '@progress/kendo-svg-icons';
+
+export default function Header() {
+  const router = useRouter();
+    return (
+    <AppBar themeColor='primary'>
+        <AppBarSection><Link href={"/"}><h1>Appointment Manager</h1></Link></AppBarSection>
+        <AppBarSpacer />
+        <AppBarSection>
+            <Button svgIcon={userIcon} fillMode={'flat'} type="button" onClick={() => router.push('/grid')} />
+        </AppBarSection>
+      </AppBar>)
+}
